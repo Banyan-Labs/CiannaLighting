24db277eaec354dc7d9172f3d7189800818c1eda
Owen T Penman <openman@banyanlabs.io>
2022-08-23 15:27:13 -0700

    some cleanup possibly required

---
+++ server/src/server.ts
@@ -0,0 +1,56 @@
+import http from 'http'
+import express from "express";
+import logging from '../config/logging'
+import config from "../config/config";
+import mongoose from 'mongoose';
+
+const router = express()
+
+/** Server Handler */
+const httpServer = http.createServer(router)
+
+mongoose
+    .connect(config.mongo.url, config.mongo.options)
+    .then(()=>{
+        logging.info('Mongoose CONNECTED.')
+    })
+    .catch((error:any)=>{
+        logging.error(error)
+    })
+
+router.use((req, res, next)=>{
+    logging.info(`METHOD: '${req.method}' - URL:'${req.url}' - IP${req.socket.remoteAddress}`);
+    res.on('finish', ()=>{
+        logging.info(`METHOD: '${req.method}' - URL:'${req.url}' - IP${req.socket.remoteAddress} - STATUS: '${res.statusCode}`)
+    });
+    next();
+});
+
+router.use(express.urlencoded({extended: false}));
+router.use(express.json());
+
+router.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method == 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
+/**Routes */
+
+/**Errors */
+router.use((req,res,next)=>{
+    const error = new Error('not found')
+
+    return res.status(404).json({
+        message: error.message
+    })
+})
+
+/**Requests */
+httpServer.listen(config.server.port, ()=>{
+    logging.info(`Server is running at ${config.server.host}:${config.server.port}`)
+})
