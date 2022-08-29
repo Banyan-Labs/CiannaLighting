2088009404899b42530a22d24568c62dc1064088
sierrascode <sierrasimental@Sierras-MacBook-Pro.local>
2022-08-29 15:24:11 -0700

    updated user check in dashboard

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -3,14 +3,21 @@ import Navbar from "../Navbar/Navbar";
 import "./style/dashboard.scss";
 
 import { AppProps } from "../../App";
+import { Navigate } from "react-router-dom";
 
 const Dashboard: FC<AppProps> = ({ user, setUser }) => {
-  return (
-    <>
-      <Navbar user={user} setUser={setUser} />
-      This is the dashboard.
-    </>
-  );
+  {
+    if (Object.keys(user).length === 0) {
+      return <Navigate to="/login" />;
+    } else {
+      return (
+        <>
+          <Navbar user={user} setUser={setUser} />
+          This is the dashboard.
+        </>
+      );
+    }
+  }
 };
 
 export default Dashboard;
