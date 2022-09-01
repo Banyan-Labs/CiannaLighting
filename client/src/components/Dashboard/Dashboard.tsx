eb2dbe62011ce905c51f21da976a508800df0511
sierrascode <sierrasimental@Sierras-MacBook-Pro.local>
2022-09-01 13:21:32 -0700

    updating dashboard

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -3,17 +3,40 @@ import "./style/dashboard.scss";
 
 import { AppProps } from "../../App";
 import { Navigate } from "react-router-dom";
-console.log('test')
+console.log("test");
 const Dashboard: FC<AppProps> = ({ user, setUser }) => {
+  const testProjectData = [
+    {
+      name: "Salt Lake Temple",
+      color: "#AC92EB",
+      status: "new",
+      description:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam …",
+      date_created: "08-02-2022",
+      activity: [],
+      rooms: [
+        { name: "", description: "", lights: [] },
+        { name: "", description: "", lights: [] },
+      ],
+      attachments: [],
+      rfp: {
+        header: "",
+        schedule: "",
+        scope: "",
+        bid: "",
+        submittals: "",
+        quality_standards: "",
+        contact_info: "",
+      },
+    },
+  ];
   return (
     <>
       {Object.keys(user).length === 0 ? (
         <Navigate to="/login" />
       ) : (
         <>
-          
-          This is the dashboard.
-          
+          <div className="dashboard-upper-section"></div>
         </>
       )}
     </>
