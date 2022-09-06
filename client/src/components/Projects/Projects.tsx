fd4d5447ed132f4b20c89baaf1874448142c9086
Matthew Scalzi <scalzi.matthew11@gmail.com>
2022-09-06 10:11:46 -0500

    hopefully fixed merge issue

--- client/src/components/Projects/Projects.tsx
+++ client/src/components/Projects/Projects.tsx
@@ -1,29 +1,27 @@
-import React, { FC } from "react";
-import ProjectAttachments from "./ProjectAttachments";
-import { useParams } from "react-router-dom";
-import Navbar from "../Navbar/Navbar";
-import ProjectSummary from "./ProjectSummary";
 import ProjectsNav from "./ProjectPageLower/ProjectsNav";
-import "./style/projects.scss";
-
-
-const Projects: FC = ({}) => {
-  const { user } = useParams();
-  
+import React, { FC } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AppProps } from '../../App';
+import ProjectSummary from './ProjectSummary';
+import ProjectAttachments from './ProjectAttachments';
+import './style/projects.scss';
+const Projects: FC<AppProps> = ({ user }) => {
   return (
     <>
-      <Navbar user={user} setUser={() => ""} />
-      <div className="projects-top-half">
-        <ProjectSummary />
-        <ProjectAttachments />
-      </div>
-      <div>
-        <ProjectsNav />
-      </div>
-     
-    
+      {Object.keys(user).length === 0 ? (
+        <Navigate to='/login' />
+      ) : (
+        <>
+          <div className='projects-top-half'>
+            <ProjectSummary />
+            <ProjectAttachments />
+          </div>
+          <div>
+            <ProjectsNav />
+          </div>
+        </>
+      )}
     </>
   );
 };
-
 export default Projects;
