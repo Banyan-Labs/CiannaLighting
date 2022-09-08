f18226bc82e7d684fe58212138d9a1cb36dd250e
sierrascode <sierrasimental@Sierras-MacBook-Pro.local>
2022-09-08 10:12:12 -0700

    updating catalog

--- client/src/components/Catalog/Catalog.tsx
+++ client/src/components/Catalog/Catalog.tsx
@@ -2,17 +2,48 @@ import React, { FC } from "react";
 import { Navigate } from "react-router-dom";
 import { AppProps } from "../../App";
 
-
 import "./style/catalog.scss";
 
+const designStylesData = [
+  "Art Deco",
+  "Colonial",
+  "Asian",
+  "Contemporary",
+  "Transitional",
+  "Traditional",
+  // "Traditional",
+];
+
+const designStyles = designStylesData.map((style) => {
+  return (
+    <>
+      <div className="design-style-buttons-container">
+        <button className="design-style-button">{style}</button>
+      </div>
+    </>
+  );
+});
+
 const Catalog: FC<AppProps> = ({ user }) => {
-  
   return (
     <>
       {Object.keys(user).length === 0 ? (
-        <Navigate to="/login"/>
+        <Navigate to="/login" />
       ) : (
-        <>This is catalog page.</>
+        <>
+          <div className="catalog-container">
+            <div className="catalog-side-container">Catalog Filter</div>
+            <div className="catalog-main-container">
+              <div className="catalog-design-styles">
+                Design Styles
+                <div className="catalog-design-styles-buttons">
+                  {designStyles}
+                </div>
+              </div>
+              <div className="catalog-use-packages">Use Packages</div>
+            </div>
+          </div>
+        </>
       )}
     </>
   );
