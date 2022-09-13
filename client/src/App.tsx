956f2ff5b5545d4e0bdaf12dab67079d55df42d9
sierrascode <sierrasimental@Sierras-MacBook-Pro.local>
2022-09-13 16:08:36 -0700

    updated navigation styling, updated default and not found route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
-import React, { FC, useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { useAppSelector } from './app/hooks';
-import Navbar from './components/Navbar/Navbar';
-import Catalog from './components/Catalog/Catalog';
-import Dashboard from './components/Dashboard/Dashboard';
-import Login from './components/Login/Login';
-import Projects from './components/Projects/Projects';
-import CreateProjectPage from './components/CreateProjectPage/CreateProjectPage';
-import './index.scss';
+import React, { FC, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAppSelector } from "./app/hooks";
+import Navbar from "./components/Navbar/Navbar";
+import Catalog from "./components/Catalog/Catalog";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Login from "./components/Login/Login";
+import Projects from "./components/Projects/Projects";
+import CreateProjectPage from "./components/CreateProjectPage/CreateProjectPage";
+import "./index.scss";
 
 export interface AppProps {
   user: any;
@@ -21,17 +21,19 @@ const App: FC = () => {
       <BrowserRouter>
         {user.isAuth && <Navbar user={user} />}
         <Routes>
-          <Route path='/login' element={<Login user={user} />} />
-          <Route path='/dashboard' element={<Navigate to='/login' />} />
-          <Route path='/catalog' element={<Navigate to='/login' />} />
-          <Route path='/projects' element={<Navigate to='/login' />} />
-          <Route path='/dashboard/:user' element={<Dashboard />} />
-          <Route path='/projects/:user' element={<Projects user={user} />} />
-          <Route path='/catalog/:user' element={<Catalog user={user} />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login user={user} />} />
+          <Route path="/dashboard" element={<Navigate to="/login" />} />
+          <Route path="/catalog" element={<Navigate to="/login" />} />
+          <Route path="/projects" element={<Navigate to="/login" />} />
+          <Route path="/dashboard/:user" element={<Dashboard />} />
+          <Route path="/projects/:user" element={<Projects user={user} />} />
+          <Route path="/catalog/:user" element={<Catalog user={user} />} />
           <Route
-            path='/create-project/:user'
+            path="/create-project/:user"
             element={<CreateProjectPage user={user} />}
           />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </>
--- client/src/components/Catalog/Catalog.tsx
+++ client/src/components/Catalog/Catalog.tsx
@@ -1,62 +1,61 @@
-import React, { FC } from 'react';
-import { Navigate } from 'react-router-dom';
-import { AppProps } from '../../App';
-import './style/catalog.scss';
-import DesignStyles from './DesignStyles/DesignStyles';
+import React, { FC } from "react";
+import { Navigate } from "react-router-dom";
+import { AppProps } from "../../App";
 
-import './style/catalog.scss';
+import DesignStyles from "./DesignStyles/DesignStyles";
+
+import "./style/catalog.scss";
 
 const usePackagesData = [
   {
     name: "Bride's Room",
-    img: '/pexels-kseniia-lopyreva-4959835.jpg',
+    img: "/pexels-kseniia-lopyreva-4959835.jpg",
   },
   {
-    name: 'Celestial Room',
-    img: '/celestial-room.jpeg',
+    name: "Celestial Room",
+    img: "/celestial-room.jpeg",
   },
   {
-    name: 'Baptistry',
-    img: '/baptistry.jpeg',
+    name: "Baptistry",
+    img: "/baptistry.jpeg",
   },
   {
-    name: 'Hallway',
-    img: '/hallway.jpeg',
+    name: "Hallway",
+    img: "/hallway.jpeg",
   },
   {
-    name: 'Reception',
-    img: '/reception.jpeg',
+    name: "Reception",
+    img: "/reception.jpeg",
   },
   {
-    name: 'Stairway',
-    img: '/stairway.jpeg',
+    name: "Stairway",
+    img: "/stairway.jpeg",
   },
   {
-    name: 'Testing',
-    img: '',
+    name: "Testing",
+    img: "",
   },
   {
-    name: 'Testing',
-    img: '',
+    name: "Testing",
+    img: "",
   },
 ];
 
-const usePackages = usePackagesData.map((usePackage) => {
+const usePackages = usePackagesData.map((usePackage, index) => {
   return (
-    <>
-      <div className='use-package-container'>
-        <div
-          className='use-package-image'
-          style={{
-            backgroundImage: `url(/images${usePackage.img})`,
-            backgroundPosition: 'top',
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-          }}
-        />
-        {usePackage.name}
-      </div>
-    </>
+    <div className="use-package-container" key={index}>
+      <div
+        className="use-package-image"
+        style={{
+          backgroundImage: `url(/images${usePackage.img})`,
+          backgroundPosition: "top",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+        key={usePackage.name}
+      />
+      {usePackage.name}
+    </div>
   );
 });
 
@@ -64,16 +63,16 @@ const Catalog: FC<AppProps> = ({ user }) => {
   return (
     <>
       {Object.keys(user).length === 0 ? (
-        <Navigate to='/login' />
+        <Navigate to="/login" />
       ) : (
         <>
-          <div className='catalog-container'>
-            <div className='catalog-main-container'>
+          <div className="catalog-container">
+            <div className="catalog-main-container">
               <DesignStyles />
 
-              <div className='catalog-use-packages'>
+              <div className="catalog-use-packages">
                 <span>Use Packages</span>
-                <div className='catalog-use-packages-buttons'>
+                <div className="catalog-use-packages-buttons">
                   {usePackages}
                 </div>
               </div>
