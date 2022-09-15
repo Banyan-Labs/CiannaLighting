5601ffc15aa16556b905db38dab60bc9d3c39112
Matthew Scalzi <scalzi.matthew11@gmail.com>
2022-09-15 11:18:29 -0500

    dashboard table

---
+++ client/src/components/Dashboard/DashboardPageLower/DashboardSubComponents/testData.js
@@ -0,0 +1,212 @@
+const testData =
+  [
+    {
+      "_id": "6320e25d22fb727580a250e9",
+      "name": "Mesa Temple",
+      "clientId": "630bb5376a52fe4f6ab8dc81",
+      "clientName": "Owen Penman",
+      "region": "south west",
+      "status": "active",
+      "description": "stuff",
+      "rooms": [
+          "6320e32422fb727580a250ee",
+          "6320e57f22fb727580a25129"
+      ],
+      "createdAt": "2022-09-13T20:04:45.107Z",
+      "updatedAt": "2022-09-13T20:18:08.099Z",
+      "__v": 4
+  },
+  {
+    "_id": "6320e25d22fb727580a250e9",
+    "name": "Mesa Temple",
+    "clientId": "630bb5376a52fe4f6ab8dc81",
+    "clientName": "Owen Penman",
+    "region": "south west",
+    "status": "active",
+    "description": "stuff",
+    "rooms": [
+        "6320e32422fb727580a250ee",
+        "6320e57f22fb727580a25129"
+    ],
+    "createdAt": "2022-09-13T20:04:45.107Z",
+    "updatedAt": "2022-09-13T20:18:08.099Z",
+    "__v": 4
+},
+{
+  "_id": "6320e25d22fb727580a250e9",
+  "name": "Mesa Temple",
+  "clientId": "630bb5376a52fe4f6ab8dc81",
+  "clientName": "Owen Penman",
+  "region": "south west",
+  "status": "active",
+  "description": "stuff",
+  "rooms": [
+      "6320e32422fb727580a250ee",
+      "6320e57f22fb727580a25129"
+  ],
+  "createdAt": "2022-09-13T20:04:45.107Z",
+  "updatedAt": "2022-09-13T20:18:08.099Z",
+  "__v": 4
+},
+{
+  "_id": "6320e25d22fb727580a250e9",
+  "name": "Mesa Temple",
+  "clientId": "630bb5376a52fe4f6ab8dc81",
+  "clientName": "Owe",
+  "region": "south west",
+  "status": "active",
+  "description": "stuff",
+  "rooms": [
+      "6320e32422fb727580a250ee",
+      "6320e57f22fb727580a25129"
+  ],
+  "createdAt": "2022-09-13T20:04:45.107Z",
+  "updatedAt": "2022-09-13T20:18:08.099Z",
+  "__v": 4
+},
+{
+  "_id": "6320e25d22fb727580a250e9",
+  "name": "Mesa Temple",
+  "clientId": "630bb5376a52fe4f6ab8dc81",
+  "clientName": " Penman",
+  "region": "south west",
+  "status": "active",
+  "description": "stuff",
+  "rooms": [
+      "6320e32422fb727580a250ee",
+      "6320e57f22fb727580a25129"
+  ],
+  "createdAt": "2022-09-13T20:04:45.107Z",
+  "updatedAt": "2022-09-13T20:18:08.099Z",
+  "__v": 4
+},
+{
+  "_id": "6320e25d22fb727580a250e9",
+  "name": "Temple",
+  "clientId": "630bb5376a52fe4f6ab8dc81",
+  "clientName": "Owen Penman",
+  "region": "south west",
+  "status": "active",
+  "description": "stuff",
+  "rooms": [
+      "6320e32422fb727580a250ee",
+      "6320e57f22fb727580a25129"
+  ],
+  "createdAt": "2022-09-13T20:04:45.107Z",
+  "updatedAt": "2022-09-13T20:18:08.099Z",
+  "__v": 4
+},
+{
+  "_id": "6320e25d22fb727580a250e9",
+  "name": "Mesa Temple",
+  "clientId": "630bb5376a52fe4f6ab8dc81",
+  "clientName": "Owen Penman",
+  "region": "south west",
+  "status": "active",
+  "description": "stuff",
+  "rooms": [
+      "6320e32422fb727580a250ee",
+      "6320e57f22fb727580a25129"
+  ],
+  "createdAt": "2022-09-13T20:04:45.107Z",
+  "updatedAt": "2022-09-13T20:18:08.099Z",
+  "__v": 4
+},
+{
+  "_id": "6320e25d22fb727580a250e9",
+  "name": "Mesa Temple",
+  "clientId": "630bb5376a52fe4f6ab8dc81",
+  "clientName": "Owen Penman",
+  "region": "south west",
+  "status": "active",
+  "description": "stuff",
+  "rooms": [
+      "6320e32422fb727580a250ee",
+      "6320e57f22fb727580a25129"
+  ],
+  "createdAt": "2022-09-13T20:04:45.107Z",
+  "updatedAt": "2022-09-13T20:18:08.099Z",
+  "__v": 4
+},
+{
+  "_id": "6320e25d22fb727580a250e9",
+  "name": "Mesa Temple",
+  "clientId": "630bb5376a52fe4f6ab8dc81",
+  "clientName": "Owen Penman",
+  "region": "south west",
+  "status": "active",
+  "description": "stuff",
+  "rooms": [
+      "6320e32422fb727580a250ee",
+      "6320e57f22fb727580a25129"
+  ],
+  "createdAt": "2022-09-13T20:04:45.107Z",
+  "updatedAt": "2022-09-13T20:18:08.099Z",
+  "__v": 4
+},
+{
+  "_id": "6320e25d22fb727580a250e9",
+  "name": "Mesa Temple",
+  "clientId": "630bb5376a52fe4f6ab8dc81",
+  "clientName": "Owen Penman",
+  "region": "south west",
+  "status": "active",
+  "description": "stuff",
+  "rooms": [
+      "6320e32422fb727580a250ee",
+      "6320e57f22fb727580a25129"
+  ],
+  "createdAt": "2022-09-13T20:04:45.107Z",
+  "updatedAt": "2022-09-13T20:18:08.099Z",
+  "__v": 4
+},
+{
+  "_id": "6320e25d22fb727580a250e9",
+  "name": "Mesa Temple",
+  "clientId": "630bb5376a52fe4f6ab8dc81",
+  "clientName": "Owen Penman",
+  "region": "south west",
+  "status": "active",
+  "description": "stuff",
+  "rooms": [
+      "6320e32422fb727580a250ee",
+      "6320e57f22fb727580a25129"
+  ],
+  "createdAt": "2022-09-13T20:04:45.107Z",
+  "updatedAt": "2022-09-13T20:18:08.099Z",
+  "__v": 4
+},
+{
+  "_id": "6320e25d22fb727580a250e9",
+  "name": "Mesa Temple",
+  "clientId": "630bb5376a52fe4f6ab8dc81",
+  "clientName": "Owen Penman",
+  "region": "south west",
+  "status": "active",
+  "description": "stuff",
+  "rooms": [
+      "6320e32422fb727580a250ee",
+      "6320e57f22fb727580a25129"
+  ],
+  "createdAt": "2022-09-13T20:04:45.107Z",
+  "updatedAt": "2022-09-13T20:18:08.099Z",
+  "__v": 4
+},
+{
+  "_id": "6320e25d22fb727580a250e9",
+  "name": "Mesa Temple",
+  "clientId": "630bb5376a52fe4f6ab8dc81",
+  "clientName": "Owen Penman",
+  "region": "south west",
+  "status": "active",
+  "description": "stuff",
+  "rooms": [
+      "6320e32422fb727580a250ee",
+      "6320e57f22fb727580a25129"
+  ],
+  "createdAt": "2022-09-13T20:04:45.107Z",
+  "updatedAt": "2022-09-13T20:18:08.099Z",
+  "__v": 4
+},
+  ]
+  export default testData
---
+++ client/src/components/Dashboard/DashboardPageLower/Pagination/Pagination.tsx
@@ -0,0 +1,34 @@
+import React, {FC} from 'react'
+interface Props {
+    projectsPerPage: number;
+    totalProjects: number;
+    paginate: any;
+}
+
+
+const Pagination: FC<Props> = ({projectsPerPage, totalProjects,paginate}:Props) => {
+    const pageNumbers = [];
+    for(let i = 1; i<=Math.ceil(totalProjects / projectsPerPage); i++){
+        pageNumbers.push(i)
+    }
+    const paginationMap = pageNumbers.map(number=>{
+        return(
+        <li key={number} className='page-item'>
+            <a href="!#" onClick={(e)=>paginate} className="page-link">
+                {number}
+            </a>
+        </li>
+        )
+    })
+  return (
+    <nav>
+        <ul className="pagination">
+        <a href="!#" className="page-link">&laquo;</a>
+         {paginationMap}
+         <a href="!#" className="page-link">&raquo;</a>
+        </ul>
+    </nav>
+  )
+}
+
+export default Pagination
