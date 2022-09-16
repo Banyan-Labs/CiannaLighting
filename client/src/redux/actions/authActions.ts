a1e532f714af089c0e668ba636b239f7c37bc700
jdickerson-banyan <75698869+jdickerson-banyan@users.noreply.github.com>
2022-09-16 08:56:31 -0700

    CIANA-15: added create user function in redux

--- client/src/redux/actions/authActions.ts
+++ client/src/redux/actions/authActions.ts
@@ -1,20 +1,27 @@
-import { Dispatch } from 'redux';
-import { createHttpRequest } from '../../api/requestTypes';
-import { setUser, setError, logout } from '../reducers/authSlice';
+import { Dispatch } from "redux";
+import { createHttpRequest } from "../../api/requestTypes";
+import { setUser, setError, logout } from "../reducers/authSlice";
 
 type SignInType = {
   email: string;
   password: string;
 };
 
-const baseUrl = 'http://localhost:1337/api/'; // will be replaced with .env variables.
+type UserType = {
+  name: String;
+  email: String;
+  password: String;
+  isAuth: boolean;
+};
+
+const baseUrl = "http://localhost:1337/api/"; // will be replaced with .env variables.
 
 export const signInAction =
   (payload: SignInType) =>
   async (dispatch: Dispatch): Promise<void> => {
     try {
       const response = await createHttpRequest(
-        baseUrl + 'users/login/user',
+        baseUrl + "users/login/user",
         payload
       );
       dispatch(setUser(response.data.User));
@@ -27,11 +34,26 @@ export const logoutAction =
   (email: string) =>
   async (dispatch: Dispatch): Promise<void> => {
     try {
-      const response = await createHttpRequest(baseUrl + 'users/log_out/user', {
+      await createHttpRequest(baseUrl + "users/log_out/user", {
         email,
       });
       dispatch(logout());
     } catch (error) {
       console.log(error);
+    }
+  };
+
+export const createUserAction =
+  (user: UserType) =>
+  async (dispatch: Dispatch): Promise<void> => {
+    try {
+      const response = await createHttpRequest(
+        baseUrl + "users/create/user",
+        user
+      );
+      dispatch(setUser(response.data.user))
+    } catch (error: any) {
+      console.log(error);
+      dispatch(setError(error.response.data));
     }
   };
