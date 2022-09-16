114d9a8c1fb79c2146749fcc8b2c400bdcda525c
jdickerson-banyan <75698869+jdickerson-banyan@users.noreply.github.com>
2022-09-16 10:58:34 -0700

    CIANA-15: rough signup component

--- client/src/components/Signup/Signup.tsx
+++ client/src/components/Signup/Signup.tsx
@@ -1,55 +1,118 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { createUserAction } from '../../redux/actions/authActions';
 
-
 const Signup: FC = () => {
-    const [newUserInput, setNewUserInput] = useState({
-        name: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-    });
-    const [errorMessage, setErrorMessage] = useState<String>('')
-    const { user } = useAppSelector(({ auth }) => auth);
-    const dispatch = useAppDispatch();
-    // const navigate = useNavigate();
+  const [newUserInput, setNewUserInput] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [errorMessage, setErrorMessage] = useState<String>('');
+  const { user } = useAppSelector(({ auth }) => auth);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-    const { name, email, password, confirmPassword } = newUserInput;
+  const { name, email, password, confirmPassword } = newUserInput;
 
-    // REMEMBER TO DELETE, THIS IS LIKELY NOT NEEDED
-    // useEffect(() => {
-    //     user.isAuth === true && navigate('/dashboard/' + user.name);
-    //   }, [user, navigate]);
+  // REMEMBER TO DELETE, THIS IS LIKELY NOT NEEDED
+  // useEffect(() => {
+  //     user.isAuth === true && navigate('/dashboard/' + user.name);
+  //   }, [user, navigate]);
 
-    const handleUserInput = (e: any) => {
-        setNewUserInput((previousState) => ({
-            ...previousState,
-            [e.target.name]:e.target.value
-        }));
-    };
+  const handleUserInput = (e: any) => {
+    setNewUserInput((previousState) => ({
+      ...previousState,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-    const registerUser = (e: any) => {
-        e.preventDefault();
-        if (!name || !email || !password || !confirmPassword) {
-            setErrorMessage('Please complete all fields')
-        } else if (password !== confirmPassword) {
-            setErrorMessage('Password does not match')
-        } else {
-            dispatch(createUserAction({
-                name,
-                email,
-                password,
-            }))
-        }
-    };
+  const registerUser = (e: any) => {
+    e.preventDefault();
+    if (!name || !email || !password || !confirmPassword) {
+      setErrorMessage('Please complete all fields');
+    } else if (password !== confirmPassword) {
+      setErrorMessage('Password does not match');
+    } else {
+      dispatch(
+        createUserAction({
+          name,
+          email,
+          password,
+        })
+      );
+    }
+  };
 
-    return (
-        <>
-            <h1>FML</h1>
-        </>
-    )
-}
+  useEffect(() => {
+    user.isAuth === true && navigate('/dashboard/' + user.name);
+  }, [user, navigate]);
 
-export default Signup
+  return (
+    <>
+      <div>
+        <form onSubmit={registerUser}>
+          <header>Sign In</header>
+          <p>Welcome! Please complete all fields</p>
+
+          <label>Name</label>
+          <br />
+          <input
+            id='name'
+            type='name'
+            name='name'
+            value={newUserInput.name}
+            onChange={(e) => handleUserInput(e)}
+            placeholder='Name'
+            required
+          />
+          <br />
+          <label>Email</label>
+          <br />
+          <input
+            id='email'
+            type='email'
+            name='email'
+            value={newUserInput.email}
+            onChange={(e) => handleUserInput(e)}
+            placeholder='Email address'
+            required
+          />
+          <br />
+          <label>Password</label>
+          <br />
+          <input
+            id='password'
+            type='password'
+            name='password'
+            value={newUserInput.password}
+            placeholder='Password'
+            onChange={(e) => handleUserInput(e)}
+            required
+          />
+          <br />
+          <label>Confirm Password</label>
+          <br />
+          <input
+            id='confirmPassword'
+            type='confirmPassword'
+            name='confirmPassword'
+            value={newUserInput.confirmPassword}
+            placeholder='Confirm Password'
+            onChange={(e) => handleUserInput(e)}
+            required
+          />
+          <br />
+          {errorMessage && <p>{errorMessage}</p>}
+          <div>
+            <button type='submit'>Sign Up</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
