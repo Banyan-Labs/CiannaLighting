b1bc89c17b06a00a00e9d20634ef972cf902daed
jdickerson-banyan <75698869+jdickerson-banyan@users.noreply.github.com>
2022-09-20 12:03:42 -0700

    Revert "CIANA-80: backend lint"
    This reverts commit da45cb93d5cea1d325c1a18b6d17ca2c4e2c7965.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,77 +1,68 @@
-import http from "http";
+import http from 'http'
 import express from "express";
-import logging from "../config/logging";
+import logging from '../config/logging'
 import config from "../config/config";
-import mongoose from "mongoose";
-import userRoutes from "./routes/userRoutes";
-import projectRoutes from "./routes/projectRoutes";
-import roomRoutes from "./routes/roomRoutes";
-import lightSelectionRoutes from "./routes/lightSelectionRoutes";
-import catalogRoutes from "./routes/catalogRoutes";
+import mongoose from 'mongoose';
+import userRoutes from './routes/userRoutes'
+import projectRoutes from './routes/projectRoutes'
+import roomRoutes from './routes/roomRoutes'
+import lightSelectionRoutes from "./routes/lightSelectionRoutes"
+import catalogRoutes from "./routes/catalogRoutes"
 
-const router = express();
+const router = express()
 
 /** Server Handler */
-const httpServer = http.createServer(router);
+const httpServer = http.createServer(router)
 
 mongoose
-  .connect(config.mongo.url, config.mongo.options)
-  .then(() => {
-    logging.info("Mongoose CONNECTED.");
-  })
-  .catch((error: any) => {
-    logging.error(error);
-  });
+    .connect(config.mongo.url, config.mongo.options)
+    .then(()=>{
+        logging.info('Mongoose CONNECTED.')
+    })
+    .catch((error:any)=>{
+        logging.error(error)
+    })
 
-router.use((req, res, next) => {
-  logging.info(
-    `METHOD: '${req.method}' - URL:'${req.url}' - IP${req.socket.remoteAddress}`
-  );
-  res.on("finish", () => {
-    logging.info(
-      `METHOD: '${req.method}' - URL:'${req.url}' - IP${req.socket.remoteAddress} - STATUS: '${res.statusCode}`
-    );
-  });
-  next();
+router.use((req, res, next)=>{
+    logging.info(`METHOD: '${req.method}' - URL:'${req.url}' - IP${req.socket.remoteAddress}`);
+    res.on('finish', ()=>{
+        logging.info(`METHOD: '${req.method}' - URL:'${req.url}' - IP${req.socket.remoteAddress} - STATUS: '${res.statusCode}`)
+    });
+    next();
 });
 
-router.use(express.urlencoded({ extended: false }));
+router.use(express.urlencoded({extended: false}));
 router.use(express.json());
 
-router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method == "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
+router.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method == 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
 
-/** Routes */
-router.use("/api/users", userRoutes);
-router.use("/api/user", userRoutes);
+/**Routes */
+router.use('/api/users', userRoutes);
+router.use('/api/user', userRoutes);
 router.use("/api/projects", projectRoutes);
 router.use("/api/rooms", roomRoutes);
 router.use("/api/lightSelector", lightSelectionRoutes);
 router.use("/api/catalog", catalogRoutes);
 
 // router.use('/api')
-/** Errors */
-router.use((req, res, next) => {
-  const error = new Error("not found");
+/**Errors */
+router.use((req,res,next)=>{
+    const error = new Error('not found')
 
-  return res.status(404).json({
-    message: error.message,
-  });
-});
+    return res.status(404).json({
+        message: error.message
+    })
+})
 
-/** Requests */
-httpServer.listen(config.server.port, () => {
-  logging.info(
-    `Server is running at ${config.server.host}:${config.server.port}`
-  );
-});
+/**Requests */
+httpServer.listen(config.server.port, ()=>{
+    logging.info(`Server is running at ${config.server.host}:${config.server.port}`)
+})
