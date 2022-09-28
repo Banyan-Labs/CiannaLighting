35948fcee4eb2c26787bbed522af5e3136fb3630
Owen T Penman <openman@banyanlabs.io>
2022-09-28 11:48:45 -0700

    status and region paths available

---
+++ server/src/model/statusAndRegion.ts
@@ -0,0 +1,9 @@
+import mongoose, { Schema } from "mongoose";
+import statusAndRegion from "../interfaces/statusAndRegion";
+
+const statusAndRegionSchema: Schema = new Schema({
+    label: {type: String},
+    value: {type: String}
+})
+
+export default mongoose.model<statusAndRegion>("statusAndRegion", statusAndRegionSchema)
