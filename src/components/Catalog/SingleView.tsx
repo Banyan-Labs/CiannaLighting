13d86feafa2def2804a45203df292c30357d4e47
carlen32 <108589578+carlen32@users.noreply.github.com>
2022-11-30 19:48:52 -0700

    single view

--- src/components/Catalog/SingleView.tsx
+++ src/components/Catalog/SingleView.tsx
@@ -8,6 +8,8 @@ interface catalogPros {
     setRenderPage:any; 
     renderPage:any
 }
+import { BsChevronLeft } from 'react-icons/bs';
+import avatar2 from '../../assets/stairway.jpeg';
 
 const SingleView: FC<catalogPros> = ({ 
     catalogItem,
@@ -17,10 +19,108 @@ const SingleView: FC<catalogPros> = ({
       setRenderPage, 
     renderPage
      }) =>  {
-
+        const Item = catalogItem
+      console.log(catalogItem)
     return (
         <div className='single-view-container'>
-          <h1>hello</h1>
+                        <p className='catalog-back' onClick={() => setCatalogItem(null)}>
+                        <BsChevronLeft onClick={() => setCatalogItem(null)} className="chevron-icon-catalog" /> Back to
+                        Catalog
+                        </p>
+          <div className='d-flex row'>
+             <div className='main-img-catalog-container col-4'>
+             <img src={avatar2} alt="img of light" />
+             </div>
+             <div className='main-img-catalog-container col-8'>
+             <h2 className="m-0">
+                        Acrylic Pendant
+                    </h2>
+             <h3>{Item?.item_ID}</h3>
+             <h4>{Item?.itemDescription}</h4>
+
+             <div className="d-flex align-items-center ">
+                            <h5 className='m-0'>
+                                Exterior Finish: 
+                            </h5>
+                              
+                                {catalogItem.exteriorFinish.map(
+                                          (
+                                              ef: string,
+                                              index = ef.indexOf(ef)
+                                          ) => {
+                                              return ( 
+                                                <div key={index} className='d-flex'>
+                                              <p>
+                                                      {ef}
+                                             </p>
+                                             </div>
+                                              );
+                                          }
+                                      )}
+                        </div>
+                        <div className="d-flex align-items-center ">
+                            <h5 className='m-0'>
+                                Exterior Finish: 
+                            </h5>
+                              
+                                {catalogItem.exteriorFinish.map(
+                                          (
+                                              ef: string,
+                                              index = ef.indexOf(ef)
+                                          ) => {
+                                              return ( 
+                                                <div key={index} className='d-flex'>
+                                              <p>
+                                                      {ef}
+                                             </p>
+                                             </div>
+                                              );
+                                          }
+                                      )}
+                        </div>
+                        <div className="d-flex align-items-center ">
+                            <h5 className='m-0'>
+                                Exterior Finish: 
+                            </h5>
+                              
+                                {catalogItem.exteriorFinish.map(
+                                          (
+                                              ef: string,
+                                              index = ef.indexOf(ef)
+                                          ) => {
+                                              return ( 
+                                                <div key={index} className='d-flex'>
+                                              <p>
+                                                      {ef}
+                                             </p>
+                                             </div>
+                                              );
+                                          }
+                                      )}
+                        </div>
+                        <div className="d-flex align-items-center ">
+                            <h5 className='m-0'>
+                                Exterior Finish: 
+                            </h5>
+                              
+                                {catalogItem.exteriorFinish.map(
+                                          (
+                                              ef: string,
+                                              index = ef.indexOf(ef)
+                                          ) => {
+                                              return ( 
+                                                <div key={index} className='d-flex'>
+                                              <p>
+                                                      {ef}
+                                             </p>
+                                             </div>
+                                              );
+                                          }
+                                      )}
+                        </div>
+             </div>
+             
+          </div>
         </div>
     );
 };
