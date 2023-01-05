524c74744ade0dd4e430ff35d09129b0718f3455
Josh McLain <joshmclain45@gmail.com>
2023-01-05 16:20:36 -0700

    making some changes

--- server/config/domainOrigins.ts
+++ server/config/domainOrigins.ts
@@ -2,4 +2,4 @@ if (process.env.NODE_ENV !== "deploy") {
   require("dotenv").config();
 }
 
-export default [process.env.ALLOWED_ORIGINS];
+export default process.env.ALLOWED_ORIGINS?.split(', ') || [''];
--- server/src/middleware/credentials.ts
+++ server/src/middleware/credentials.ts
@@ -3,7 +3,7 @@ import allowedOrigins from "../../config/domainOrigins";
 
 const credentials = (req: Request, res: any, next: NextFunction) => {
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
+  if (origin && allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
   }
   next();
