edae1a7b6c92c70e2149e5861b1488804d2d7abf
Owen T Penman <openman@banyanlabs.io>
2023-01-11 07:03:11 -0700

    all duplicating should be functioning

--- client/src/components/Dashboard/DashboardPageLower/DashboardSubComponents/ProjectMiniModal.tsx
+++ client/src/components/Dashboard/DashboardPageLower/DashboardSubComponents/ProjectMiniModal.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
-import { useAppSelector } from '../../../../app/hooks';
+import { useAppSelector, useAppDispatch } from '../../../../app/hooks';
 import './style/allProjects.scss';
 import { FaRegCopy, FaRegEye, FaTrash } from 'react-icons/fa';
 import { ROLES } from '../../../../app/constants';
+import {axiosPrivate} from "../../../../api/axios"
+import {createProjectAction, getUserProjects, getAllProjects} from "../../../../redux/actions/projectActions"
 
 interface projectProps {
     setOpenModal: any;
@@ -18,9 +20,43 @@ const ProjectMiniModal: FC<projectProps> = ({
     setDeleteProject,
 }) => {
     const { user } = useAppSelector(({ auth: user }) => user);
+    const dispatch = useAppDispatch();
+    const copyOfProject = async (e: any) => {
+        e.preventDefault();
+        // FIND PROJECT WITH AXIOS
+        const axiosPriv = await axiosPrivate();
+        
+        const attach = await axiosPriv.post('/get-attachments', {
+            projId: project._id,
+        });
+        let attachments = [];
+        if (attach) {
+            console.log('ATTACH: ', attach);
+            attachments = attach.data.proj.pdf;
+            if (attachments.length) {
+                const payload = {
+                    project: {...project, clientId: user._id, clientName: user.name},
+                    copy: 'project',
+                    attachments: attachments,
+                };
+                try {
+                    const response = await dispatch(
+                        createProjectAction(payload)
+                    );
+                    dispatch(getUserProjects(user._id));
+                    dispatch(getAllProjects());
+                    alert(`Copy of ${project.name} created in your dashboard.`);
+                    return response;
+                } catch (error) {
+                    console.log('Error in copyProject: ', error);
+                    return error
+                }
+            }
+        }
+    };
     return (
         <div className="project-mini-modal">
-            <div className="project-mini-modal-link">
+            <div className="project-mini-modal-link" onClick={(e)=> copyOfProject(e)}>
                 <FaRegCopy />
                 <p>Duplicate</p>
             </div>
