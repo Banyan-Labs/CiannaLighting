90427c19111c92302a298cffb6a02f3ba97c5fa7
Josh McLain <joshmclain45@gmail.com>
2023-02-03 23:48:26 -0700

    wtf

--- deploy-stg.sh
+++ deploy-stg.sh
@@ -2,8 +2,7 @@
 echo Building Cianna Lighting web app
 
 echo Fetching most recent changes on stage branch
-git checkout stage
-git pull
+git checkout stage && git pull
 
 echo Updating node modules
 npm run install-modules
