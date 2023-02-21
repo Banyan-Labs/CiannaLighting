91e052066ded342ef08bbfc9b37339fa0c2180a8
Josh McLain <joshmclain45@gmail.com>
2023-02-21 15:22:42 -0700

    light carousel and styling

---
+++ client/src/components/Rooms/LightSide/LightCarousel.tsx
@@ -0,0 +1,102 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
+import './carousel.style.scss';
+
+export type ImageType = { id: number; url: string };
+type Props = {
+    images?: ImageType[];
+};
+
+const LightCarousel = ({ images }: Props) => {
+    const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+    const [selectedImage, setSelectedImage] = useState<ImageType>();
+    const carouselItemsRef = useRef<HTMLDivElement[] | null[]>([]);
+
+    useEffect(() => {
+        if (images && images[0]) {
+            carouselItemsRef.current = carouselItemsRef.current.slice(
+                0,
+                images.length
+            );
+
+            setSelectedImageIndex(0);
+            setSelectedImage(images[0]);
+        }
+    }, [images]);
+
+    const handleSelectedImageChange = (newIdx: number) => {
+        if (images && images.length > 0) {
+            setSelectedImage(images[newIdx]);
+            setSelectedImageIndex(newIdx);
+            if (carouselItemsRef?.current[newIdx]) {
+                carouselItemsRef?.current[newIdx]?.scrollIntoView({
+                    inline: 'center',
+                    behavior: 'smooth',
+                });
+            }
+        }
+    };
+
+    const handleRightClick = () => {
+        if (images && images.length > 0) {
+            let newIdx = selectedImageIndex + 1;
+            if (newIdx >= images.length) {
+                newIdx = 0;
+            }
+            handleSelectedImageChange(newIdx);
+        }
+    };
+
+    const handleLeftClick = () => {
+        if (images && images.length > 0) {
+            let newIdx = selectedImageIndex - 1;
+            if (newIdx < 0) {
+                newIdx = images.length - 1;
+            }
+            handleSelectedImageChange(newIdx);
+        }
+    };
+
+    return (
+        <div className="carousel-container">
+            <div className="img-wrapper">
+                <img className="selected-image" src={selectedImage?.url} />
+            </div>
+            <div className="carousel">
+                <div className="carousel__images">
+                    {images &&
+                        images.map((image, idx) => (
+                            <div
+                                onClick={() => handleSelectedImageChange(idx)}
+                                style={{ backgroundImage: `url(${image.url})` }}
+                                key={image.id}
+                                className={`carousel__image ${
+                                    selectedImageIndex === idx &&
+                                    'carousel__image-selected'
+                                }`}
+                                ref={(el) =>
+                                    (carouselItemsRef.current[idx] = el)
+                                }
+                            />
+                        ))}
+                </div>
+                <button
+                    type="button"
+                    className="carousel__button carousel__button-left"
+                    onClick={handleLeftClick}
+                >
+                    <FaChevronLeft />
+                </button>
+                <button
+                    type="button"
+                    className="carousel__button carousel__button-right"
+                    onClick={handleRightClick}
+                >
+                    <FaChevronRight />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default LightCarousel;
