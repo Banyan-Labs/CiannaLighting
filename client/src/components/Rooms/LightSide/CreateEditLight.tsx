d0f48665b41717595c3da90e4c56e1015fd3cb99
Josh McLain <joshmclain45@gmail.com>
2023-03-16 13:14:10 -0700

    responsiveness

--- client/src/components/Rooms/LightSide/CreateEditLight.tsx
+++ client/src/components/Rooms/LightSide/CreateEditLight.tsx
@@ -35,7 +35,7 @@ const CreateEditLight = ({
     const handleNavigationClick = useCallback(() => returnToNull(), []);
 
     return (
-        <div className="light-details__container">
+        <div className="light-details">
             <div className="light-details__heading">
                 <BreadCrumb
                     stateAction={handleNavigationClick}
@@ -50,28 +50,31 @@ const CreateEditLight = ({
                     </span>
                 )}
             </div>
-
-            <div className="light-details__carousel-wrapper">
-                <LightImageCarousel images={carouselImageData} />
-                <LightSpecifications lightDetails={catalogItem} />
-            </div>
-            <div className="light-details__description-wrapper">
-                <h2 className="light-details__item-name">
-                    {catalogItem.itemName}
-                </h2>
-                <h3 className="light-details__item-id">
-                    {catalogItem.item_ID}
-                </h3>
-                <p className="light-details__description-text">
-                    {catalogItem.itemDescription}
-                </p>
-                <LightOptionsForm
-                    catalogLightItem={catalogItem}
-                    editLightItem={editLight}
-                    setCatalogItem={setCatalogItem}
-                    setEditLight={setEditLight}
-                    lightSpecs={catalogItem.specs}
-                />
+            <div className="light-details__content">
+                <div className="light-details__carousel-wrapper">
+                    <LightImageCarousel images={carouselImageData} />
+                    <LightSpecifications lightDetails={catalogItem} />
+                </div>
+                <div className="light-details__description-wrapper">
+                    <h2 className="light-details__item-name">
+                        {catalogItem.itemName}
+                    </h2>
+                    <h3 className="light-details__item-id">
+                        {catalogItem.item_ID}
+                    </h3>
+                    <p className="light-details__description-text">
+                        {catalogItem.itemDescription}
+                    </p>
+                    <div className="light-details__form-wrapper">
+                        <LightOptionsForm
+                            catalogLightItem={catalogItem}
+                            editLightItem={editLight}
+                            setCatalogItem={setCatalogItem}
+                            setEditLight={setEditLight}
+                            lightSpecs={catalogItem.specs}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
