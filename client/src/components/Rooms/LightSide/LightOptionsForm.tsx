cc8fde18e25cb310f53ed0d5887068a597e5774b
David Ditto <dditto@vant4ge.com>
2023-07-06 21:34:22 -0500

    david-1 : Refactored create light logic to include drawing files and pdf files from catalog item.

--- client/src/components/Rooms/LightSide/LightOptionsForm.tsx
+++ client/src/components/Rooms/LightSide/LightOptionsForm.tsx
@@ -69,23 +69,26 @@ function LightOptionsForm({
         try {
             if (!editLight) {
                 if (lightSpecs.length) {
+                    const pdfFiles = [...lightSpecs, ...catalogLight.pdf, ...catalogLight.drawingFiles];
+
                     dispatch(
                         setSpecFile(
                             {
                                 projId: projectId,
-                                pdf: lightSpecs,
+                                pdf: pdfFiles,
                                 images: [
                                     {
                                         lightId: lightID,
-                                        attachments: lightSpecs,
+                                        attachments: catalogLight.images,
                                     },
                                 ],
                                 edit: 'add',
                             },
-                            attachments.length > 0 ? false : true
+                            pdfFiles.length > 0 || catalogLight.images.length > 0
                         )
                     );
                 }
+
                 await dispatch(
                     createLight({ ...catalogLight, ...rfpPassData })
                 );
